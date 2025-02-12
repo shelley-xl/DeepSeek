957d567dd31c6dee06a1dabb3ce2ab64ecdcbc6b
xulai <386710057@qq.com>
2025-02-12 16:16:29 +0800

    示例项目

---
+++ simples/DeepSeek.ApiSimple/Program.cs
@@ -0,0 +1,65 @@
+﻿var services = new ServiceCollection();
+
+var configuration = new ConfigurationBuilder()
+   .SetBasePath(AppContext.BaseDirectory)
+   .AddJsonFile($"appsettings.json", true, true)
+   .Build();
+
+services.AddSingleton<IConfiguration>(configuration);
+services.AddDeepSeekClient();
+
+var provider = services.BuildServiceProvider();
+
+var deepSeekService = provider.GetRequiredService<IDeepSeekService>();
+
+while (true)
+{
+    Console.WriteLine();
+    Console.Write("请输入：");
+
+    var input = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(input))
+    {
+        continue;
+    }
+
+    Console.WriteLine();
+    Console.Write("正在思考，请稍后...");
+    Console.WriteLine();
+    Console.WriteLine();
+
+    var request = new ApiChatRequest
+    {
+        Messages = [Message.NewUserMessage(input)],
+        Model = Constants.Models.ChatModel,
+    };
+
+    var cancellationToken = new CancellationTokenSource();
+
+    var choices = deepSeekService.ApiChatStreamAsync(request, cancellationToken.Token);
+
+    await foreach (var choice in choices)
+    {
+        Console.Write(choice?.Delta?.Content);
+    }
+
+    if (deepSeekService.ErrorMessage is not null)
+    {
+        Console.WriteLine(deepSeekService.ErrorMessage);
+        continue;
+    }
+
+    Console.WriteLine();
+
+    var balance = await deepSeekService.ApiGetUserBalanceAsync(cancellationToken.Token);
+    
+    if (deepSeekService.ErrorMessage is not null)
+    {
+        Console.WriteLine(deepSeekService.ErrorMessage);
+        continue;
+    }
+
+    Console.WriteLine();
+    Console.WriteLine($"账户余额：{balance?.BalanceInfos?.FirstOrDefault()?.TotalBalance}");
+}
---
+++ simples/DeepSeek.OllamaSimple/Program.cs
@@ -0,0 +1,49 @@
+﻿var services = new ServiceCollection();
+
+services.AddDeepSeekClient();
+
+var provider = services.BuildServiceProvider();
+
+var deepSeekService = provider.GetRequiredService<IDeepSeekService>();
+
+while (true)
+{
+    Console.WriteLine();
+    Console.Write("请输入：");
+
+    var input = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(input))
+    {
+        continue;
+    }
+
+    Console.WriteLine();
+    Console.Write("正在思考，请稍后...");
+    Console.WriteLine();
+    Console.WriteLine();
+
+    var request = new OllamaChatRequest
+    {
+        ModelId = Constants.OllamaModels.DeepSeek_R1_7b,
+        BaseUrl = "http://localhost:11434",
+        PromptTemplate = input,
+    };
+
+    var cancellationToken = new CancellationTokenSource();
+
+    var response = deepSeekService.OllamaChatStreamingAsync(request, cancellationToken.Token);
+
+    await foreach (var result in response)
+    {
+        Console.Write(result);
+    }
+
+    if (deepSeekService.ErrorMessage is not null)
+    {
+        Console.WriteLine(deepSeekService.ErrorMessage);
+        continue;
+    }
+
+    Console.WriteLine();
+}
