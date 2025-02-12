4bfec5ec3609eef8a9c8b669be71e0388b11dec8
xulai <386710057@qq.com>
2025-02-12 11:48:51 +0800

    优化

--- src/Assembly/DeepSeekAssembly.cs
+++ src/Assembly/DeepSeekAssembly.cs
@@ -2,10 +2,10 @@
 global using Microsoft.Extensions.DependencyInjection;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
-global using System.Runtime.CompilerServices;
 global using System.Text;
 global using System.Text.Encodings.Web;
 global using System.Text.Unicode;
+global using System.Runtime.CompilerServices;
 global using System.Net.Http.Json;
 global using DeepSeek.Core;
 global using DeepSeek.Dtos;
